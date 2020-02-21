0deead3614fc700a58705726a94b6b84014c00b0
linhhm24 <linh.hm24@gmail.com>
2020-02-21 15:27:52 +0700

    add file

---
+++ app.js
@@ -0,0 +1,9 @@
+function pushno() {
+    document.getElementById("no").style.left = Math.floor(Math.random()*1000)+'px';
+    document.getElementById("no").style.top = Math.floor(Math.random()*500)+'px';
+
+}
+
+function pushyes() {
+    alert('hihi');
+}
